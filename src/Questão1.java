59f033bf94cd54c3e760b327f655d2d39af2e3b4
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-02-08 12:44:43 -0300

    resolução da questão 1

---
+++ src/Questão1.java
@@ -0,0 +1,9 @@
+public class Questão1 {
+
+    public static void main(String[] args) {
+
+        String nome = "João Vitor";
+
+        System.out.println("Olá," + nome + "!" + "Bem-vindo ao nosso programa!.");
+    }
+}
