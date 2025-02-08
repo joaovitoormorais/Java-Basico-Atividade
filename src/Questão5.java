615288f791f92b0279013361709c6b149dac73a2
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-02-08 13:36:59 -0300

    resolução da 5 questão

--- src/Questão5.java
+++ src/Questão5.java
@@ -4,8 +4,27 @@ public class Questão5 {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    
+        double valorEmprestimo, taxaJuros, valorTotal, valorParcela;
+        int qtdMeses;
 
-        
+        System.out.println("Entre com o valor do empréstimo:");
+        valorEmprestimo = scan.nextDouble();
+
+        System.out.println("Qual a taxa de juros mensal?");
+        taxaJuros = scan.nextDouble();
+
+        System.out.println("O empréstimo foi em quantos meses?");
+        qtdMeses = scan.nextInt();
+
+//juros simples
+        valorTotal = valorEmprestimo * (taxaJuros / 100) * qtdMeses;
+
+        valorParcela = valorTotal / qtdMeses;
+
+        System.out .println("O valor Total com juros é R$ " + valorTotal);
+
+        System.out .println("O valor de cada parcela será de R$ " + valorParcela);
     }
 }
