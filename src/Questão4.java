859dbda37106daef26df9929c84f9ef6ce68b653
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-02-08 13:20:32 -0300

    resolução da 4 questão

--- src/Questão4.java
+++ src/Questão4.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Questão4 {
 
     public static void main(String[] args) {
-        
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o número de horas trabalhadas pelo funcionário:");
+        int qtdHoras = scan.nextInt();
+
+        System.out.println("Informe o valor da hora em R$ recebida pelo funcionário:");
+        int valorHora = scan.nextInt();
+
+        double salBruto = qtdHoras * valorHora;
+
+        System.out.println("O salário bruto do trabalhado em R$ foi de:" + salBruto);
     }
 }
---
+++ src/Questão5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Questão5 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        
+    }
+}
